9af203c536d22759f785acfa4b0e070775ab553f
unknown <alonso.tarazona@tecsup.edu.pe>
2024-09-17 13:47:34 -0500

    test

---
+++ Assets/Scripts/CardInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "MemoryGame/Card")]
+
+public class CardInfo : ScriptableObject
+{
+    public int index;
+    public Sprite sprite;
+}
